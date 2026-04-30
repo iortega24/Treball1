cbe86452924b7d73089f2e20efb69451f7fe986c
ian <iortegaol@esliceu.net>
2026-04-30 13:51:02 +0200

    script21.sh 1

---
+++ script21.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#es mostra l'script executat
+echo "Nom del script ${0}"
+
+#mostram el PATH (RUTA) I el filename del script
+echo "el PATH: $(dirname ${0}) el fitxer seria $(basename ${0})"
+
+#per determinar el número de paràmetre
+NUMERO_DE_PARAMETRES=${#}
+
+if [[ ${NUMERO_DE_PARAMETRES} -eq 0 ]]
+then
+ echo "USAGE: ${0} USER_NAME [USER_NAME] ..."
+ exit 1
+fi
+
+#${*} mostra el conjunt de paràmetres en una sola paraula "${1} ${2}.....${n}"
+echo "${*}"
+echo
+#${@} mostra el conjunt de paràmetres en paraules diferents "${1}" "${2}" ..... "${n}"
+echo "${@}"
+
+#estructura de control FOR per interar els paràmetres
+for USER_NAME in "${@}"
+do
+ PASSWORD=$(date +%s%N | sha256sum | head -c10)
+ echo "${USER_NAME}:${PASSWORD}"
+done
+
+X=1
+while [[ ${X} -eq 1 ]]
+do
+ echo "executa while"
+ X=7
+done
