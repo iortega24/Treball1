c234811decd2d5e2b6cc5fb10ab53a9f8cd3dcf3
ian <iortegaol@esliceu.net>
2026-05-04 11:58:15 +0200

    script22.sh 3

--- script22.sh
+++ script22.sh
@@ -5,16 +5,23 @@
 #starting, stopping, the status is ....
 #si no és cap d'aquests valors la sortida serà: valor no vàlid....
 
-if [[ "${1}" = "start" ]]
-then
- echo "Starting...."
-elif [[ "${1}" = "stop" ]]
-then
- echo "Stopping...."
-elif [[ "${1}" = "status" ]]
-then
- echo "The status is...."
-else
- echo "Enter a valid option...."
-  
- fi
+#if [[ "${1}" = "start" ]]
+#then
+# echo "Starting...."
+#elif [[ "${1}" = "stop" ]]
+#then
+# echo "Stopping...."
+#elif [[ "${1}" = "status" ]]
+#then
+# echo "The status is...."
+#else
+# echo "Enter a valid option...."
+#
+#fi
+
+#ho farem amb CASE
+case "${1}" in
+    start)
+    echo "starting..."
+    ;;
+esac
